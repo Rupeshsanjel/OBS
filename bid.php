b8680218f0bebadef65095f9227fca46a2667868
Rupesh <sanjelrupesh2@gmail.com>
2021-12-08 21:51:47 +0545

    obs

---
+++ bid.php
@@ -0,0 +1,15 @@
+<?php
+include "header.php";
+?>
+
+
+<section class="section">
+<div class="container-fluid">	
+    <div id="bid_checkout">
+    </div>
+</div>
+</section>	
+<?php
+include "newslettter.php";
+include "footer.php";
+?>
